4b96759aa33ad46a310896f7b2e87d68bbb6a8dd
socmutbv <82619963+socmutbv@users.noreply.github.com>
2021-04-19 22:54:38 +0200

    Update hello_pli.groovy

--- Build/HelloWorld/hello_pli.groovy
+++ Build/HelloWorld/hello_pli.groovy
@@ -1,29 +1,29 @@
 import com.ibm.dbb.build.*
 
 // Change the following variables to match your system
-hlq        = "USER.BUILD"
-sourceDir  = "/u/user/build"
-compilerDS = "IBMZ.V5R2M0.SIBMZCMP"
+hlq        = "TSATBV"
+sourceDir  = "/u/tsatbv/DBB_Sandbox"
+compilerDS = "IBMZ.SIBMZCMP"
 
-println("Creating ${hlq}.PLI. . .")
+println("Creating ${hlq}.SOURCE.DBB.PDS. . .")
 CreatePDS createPDSCmd = new CreatePDS();
-createPDSCmd.setDataset("${hlq}.PLI");
+createPDSCmd.setDataset("${hlq}.SOURCE.DBB.PDS");
 createPDSCmd.setOptions("tracks space(1,1) lrecl(80) dsorg(PO) recfm(F,B) dsntype(library)");
 createPDSCmd.create();
 
-println("Creating ${hlq}.OBJ. . .")
-createPDSCmd.setDataset("${hlq}.OBJ");
+println("Creating ${hlq}.OBJ.DBB.PDS. . .")
+createPDSCmd.setDataset("${hlq}.OBJ.DBB.PDS");
 createPDSCmd.setOptions("tracks space(1,1) lrecl(80) dsorg(PO) recfm(F,B) dsntype(library)");
 createPDSCmd.create();
 
-println("Copying ${sourceDir}/hello.pli to ${hlq}.PLI(HELLO) . . .")
-def copy = new CopyToPDS().file(new File("${sourceDir}/hello.pli")).dataset("${hlq}.PLI").member("HELLO")
+println("Copying ${sourceDir}/hello.pli to ${hlq}.SOURCE.DBB.PDS(HELLO) . . .")
+def copy = new CopyToPDS().file(new File("${sourceDir}/hello.pli")).dataset("${hlq}.SOURCE.DBB.PDS").member("HELLO")
 copy.execute()
 
-println("Compiling ${hlq}.PLI(HELLO). . .")
+println("Compiling ${hlq}.SOURCE.DBB.PDS(HELLO). . .")
 def compile = new MVSExec().pgm("IBMZPLI").parm("SOURCE")
-compile.dd(new DDStatement().name("SYSIN").dsn("${hlq}.PLI(HELLO)").options("shr"))
-compile.dd(new DDStatement().name("SYSLIN").dsn("${hlq}.OBJ(HELLO)").options("shr"))
+compile.dd(new DDStatement().name("SYSIN").dsn("${hlq}.SOURCE.DBB.PDS(HELLO)").options("shr"))
+compile.dd(new DDStatement().name("SYSLIN").dsn("${hlq}.OBJ.DBB.PDS(HELLO)").options("shr"))
 compile.dd(new DDStatement().name("SYSUT1").options("cyl space(5,5) unit(vio)  new"))
 compile.dd(new DDStatement().name("SYSTERM").options("cyl space(5,5) unit(vio)  new"))
 compile.dd(new DDStatement().name("SYSPUNCH").options("cyl space(5,5) unit(vio)  new"))
